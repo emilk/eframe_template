6a6611d0b5a7a7cc96d9105dbe4e06657d09ec20
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-08-23 22:32:44 +0200

    Add egui_template.js to sw.js for PWA
    Builds on https://github.com/emilk/egui_template/pull/15

--- docs/sw.js
+++ docs/sw.js
@@ -2,23 +2,23 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './egui_template_bg.wasm'
+  './egui_template.js',
+  './egui_template_bg.wasm',
 ];
 
-
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
