71211e01104c91da883b75ff88e5c5a839c44d89
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-12-28 21:29:52 +0100

    setup_web.sh: don't force-install wasm-bindgen-cli

--- setup_web.sh
+++ setup_web.sh
@@ -3,7 +3,7 @@ set -eu
 
 # Pre-requisites:
 rustup target add wasm32-unknown-unknown
-cargo install -f wasm-bindgen-cli
+cargo install wasm-bindgen-cli
 cargo update -p wasm-bindgen
 
 # For local tests with `./start_server`:
