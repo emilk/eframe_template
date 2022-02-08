009efff9960f4cac53ac45381da9aaa7f6fdc6f5
Austen Adler <agadler@austenadler.com>
2022-02-07 22:23:26 -0500

    Quote all shell variables

--- build_web.sh
+++ build_web.sh
@@ -40,11 +40,11 @@ CRATE_NAME_SNAKE_CASE="${CRATE_NAME//-/_}" # for those who name crates with-keba
 export RUSTFLAGS=--cfg=web_sys_unstable_apis
 
 # Clear output from old stuff:
-rm -f docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm
+rm -f "docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm"
 
 echo "Building rust…"
 BUILD=release
-cargo build -p ${CRATE_NAME} --release --lib --target wasm32-unknown-unknown
+cargo build -p "${CRATE_NAME}" --release --lib --target wasm32-unknown-unknown
 
 # Get the output directory (in the workspace it is in another location)
 TARGET=`cargo metadata --format-version=1 | jq --raw-output .target_directory`
@@ -57,7 +57,7 @@ wasm-bindgen "${TARGET}/wasm32-unknown-unknown/${BUILD}/${TARGET_NAME}" \
 if [ "${FAST}" = false ]; then
   echo "Optimizing wasm…"
   # to get wasm-opt:  apt/brew/dnf install binaryen
-  wasm-opt docs/${CRATE_NAME}_bg.wasm -O2 --fast-math -o docs/${CRATE_NAME}_bg.wasm # add -g to get debug symbols
+  wasm-opt "docs/${CRATE_NAME}_bg.wasm" -O2 --fast-math -o "docs/${CRATE_NAME}_bg.wasm" # add -g to get debug symbols
 fi
 
 echo "Finished: docs/${CRATE_NAME_SNAKE_CASE}.wasm"
