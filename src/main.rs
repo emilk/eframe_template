86fe7b7b87e3a3868ce2648a3f2a63b6a044133f
NHodgesVFX <NhodgesVFX@gmail.com>
2022-03-22 03:31:14 -0400

    Hide console window in release builds on Windows (#53)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
 #![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] //Hide console window in release builds on Windows, this blocks stdout.
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
