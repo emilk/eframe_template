2b76b1b047a8029e1bf58436aa93be20238154c0
Alex Kazik <alex@kazik.de>
2022-07-07 18:55:29 +0200

    Remove duplicate cargo install
    It's already executed in setup_web.sh.

--- start_server.sh
+++ start_server.sh
@@ -4,8 +4,6 @@ set -eu
 # Starts a local web-server that serves the contents of the `doc/` folder,
 # which is the folder to where the web version is compiled.
 
-cargo install basic-http-server
-
 echo "open http://localhost:8080"
 
 (cd docs && basic-http-server --addr 127.0.0.1:8080 .)
