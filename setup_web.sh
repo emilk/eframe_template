bdf7b0e2c77b5cec0bc7e36fc9d6505b5dfea6cc
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-08-02 09:48:44 +0200

    cargo update

--- setup_web.sh
+++ setup_web.sh
@@ -3,7 +3,7 @@ set -eu
 
 # Pre-requisites:
 rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli --version 0.2.81
+cargo install wasm-bindgen-cli --version 0.2.82
 
 # For local tests with `./start_server`:
 cargo install basic-http-server
