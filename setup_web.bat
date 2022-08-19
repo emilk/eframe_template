d12c19da37de943d06703bec09613cf7121a70f9
Red Artist <redartofcode@gmail.com>
2022-08-19 21:03:23 +0530

    Use Trunk to build and deploy the web app (#69)

--- setup_web.bat
+++
@@ -1,7 +0,0 @@
-@REM Pre-requisites:
-rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli
-cargo update -p wasm-bindgen
-
-@REM For local tests with `./start_server`:
-cargo install basic-http-server
--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,3 @@
 
 mod app;
 pub use app::TemplateApp;
-
-// ----------------------------------------------------------------------------
-// When compiling for web:
-
-#[cfg(target_arch = "wasm32")]
-use eframe::wasm_bindgen::{self, prelude::*};
-
-/// This is the entry-point for all the web-assembly.
-/// This is called once from the HTML.
-/// It loads the app, installs some callbacks, then returns.
-/// You can add more callbacks like this if you want to call in to your code.
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub fn start(canvas_id: &str) -> Result<(), eframe::wasm_bindgen::JsValue> {
-    // Make sure panics are logged using `console.error`.
-    console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log and friends:
-    tracing_wasm::set_as_global_default();
-
-    eframe::start_web(canvas_id, Box::new(|cc| Box::new(TemplateApp::new(cc))))
-}
