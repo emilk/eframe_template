a57257c5d312abdf85dc6b550810c0a94245e6b7
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-11-23 16:31:10 +0100

    Update to egui 0.24

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
-        initial_window_size: Some([400.0, 300.0].into()),
-        min_window_size: Some([300.0, 220.0].into()),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
         ..Default::default()
     };
     eframe::run_native(
