c78e339fb7445b1b11f6c12f5f7a7421390eaa3f
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-12-29 19:06:00 +0300

    Update app.rs (#118)
    seems there is no TopPanel but TopBottomPanel

--- src/app.rs
+++ src/app.rs
@@ -43,7 +43,7 @@ impl eframe::App for TemplateApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
