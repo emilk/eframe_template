27c46aee686fddda651adc1bf611812d0e6f241e
One <43485962+c-git@users.noreply.github.com>
2024-01-09 20:46:06 -0500

    Provide code to user to show how to set icon

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .unwrap(),
+            ),
         ..Default::default()
     };
     eframe::run_native(
