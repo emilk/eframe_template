02ee968537e783e375a0e439426518bf91e762b6
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-02-05 17:40:21 +0100

    Update to egui and eframe 0.26

--- check.sh
+++ check.sh
@@ -2,10 +2,10 @@
 # This scripts runs various CI-like checks in a convenient way.
 set -eux
 
-cargo check --workspace --all-targets
-cargo check --workspace --all-features --lib --target wasm32-unknown-unknown
+cargo check --quiet --workspace --all-targets
+cargo check --quiet --workspace --all-features --lib --target wasm32-unknown-unknown
 cargo fmt --all -- --check
-cargo clippy --workspace --all-targets --all-features --  -D warnings -W clippy::all
-cargo test --workspace --all-targets --all-features
-cargo test --workspace --doc
+cargo clippy --quiet --workspace --all-targets --all-features --  -D warnings -W clippy::all
+cargo test --quiet --workspace --all-targets --all-features
+cargo test --quiet --workspace --doc
 trunk build
