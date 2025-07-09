549f881237492a4e265ec8fe66fccadcfda6e812
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-07-09 16:39:18 +0200

    Make TemplateApp::new nicer
    Closes https://github.com/emilk/eframe_template/issues/199

--- src/app.rs
+++ src/app.rs
@@ -28,10 +28,10 @@ impl TemplateApp {
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+            eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
+        } else {
+            Default::default()
         }
-
-        Default::default()
     }
 }
 
