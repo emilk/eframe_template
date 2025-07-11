d734e7ef4b4066d1de54468503ccdf73959c6390
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-07-11 10:28:54 +0200

    Update to egui 0.32 (#203)
    Awaiting release of 0.32
    
    ---------
    
    Co-authored-by: lucasmerlin <hi@lucasmerlin.me>

--- src/app.rs
+++ src/app.rs
@@ -49,7 +49,7 @@ impl eframe::App for TemplateApp {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
 
-            egui::menu::bar(ui, |ui| {
+            egui::MenuBar::new().ui(ui, |ui| {
                 // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
                 if !is_web {
